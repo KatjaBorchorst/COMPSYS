a723d76ce4054368a7285e8bdc46dffdda3285a2
cfp185 <cfp185@alumni.ku.dk>
2022-10-05 14:38:31 +0200

    id_query_naive.c working

--- src/id_query_naive.c
+++ src/id_query_naive.c
@@ -23,21 +23,10 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  struct naive_data *arr = malloc(sizeof(struct naive_data));
+  struct naive_data* arr = malloc(sizeof(struct naive_data));
   arr->n = n;
   arr->rs = rs;
   return arr;
-
-  // n = 19999;
-  // for (i = 1; i < n; ++i) {
-  //   printf("housenumber: %lld \n", rs[i].osm_id); 
-  //   printf("lon: %f \n", rs[i].lon);
-  //   printf("lat: %f \n", rs[i].lat);
-  //   printf("name: %s \n", rs[i].name);
-  //   printf("data: %s %d %d \n", rs[i].name, rs[i].lat, rs[i].lon);
-  //   printf("\n");
-  // }
-  // return 0;
 }
 
 void free_naive(struct naive_data* data) {
@@ -48,25 +37,16 @@ void free_naive(struct naive_data* data) {
     free(data->rs);
   }
   free(data);
-  // free(data->rs);
-	// free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
-  // int c = 20000;
-  // int i;
   struct record *rs = data->rs;
-   for (int i = 0; i < data->n; ++i) {
-    int64_t osm_id = rs[i].osm_id;
-    //printf("%ld: %s %f %f \n", rs[i].osm_id, rs[i].name, rs[i].lat, rs[i].lon);
-     if (osm_id == needle) {
-      
-      return rs;
-       //printf("%ld: %lld %d %d \n", data, curr[i].name, curr[i].lat, curr[i].lon); //NUMBER: name, osm_id, coordinates
+  for (int i = 0; i < data->n; ++i) {
+    //int64_t osm_id = rs[i].osm_id;
+     if (rs[i].osm_id == needle) {      
+      return &data->rs[i];
      }
-   }
-  // //assert(0);
-  printf("Element not found");
+   } printf("Element not found");
 	return NULL;
 
 }
