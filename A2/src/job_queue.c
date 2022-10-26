9e254c18c4c046f800769321457ed211b2f3d9c7
KatjaBorchorst <wkl712@alumni.ku.dk>
2022-10-26 13:43:54 +0200

    changes to queue

--- A2/src/job_queue.c
+++ A2/src/job_queue.c
@@ -15,8 +15,8 @@ int job_queue_init(struct job_queue *job_queue, int capacity) {
   }
   job_queue->capacity = capacity;
   job_queue->size = 0;
-  job_queue->top = -1;
-  job_queue->bottom = -1;
+  job_queue->top = 0;
+  job_queue->bottom = 0;
   job_queue->die = 0;
   return 0;
 }
@@ -31,27 +31,26 @@ int job_queue_destroy(struct job_queue *job_queue) {
   assert(pthread_cond_broadcast (&filled_cond) == 0);
   assert(pthread_cond_destroy(&filled_cond) == 0);
   assert (pthread_cond_destroy(&empty_cond) == 0);
-  assert(pthread_mutex_unlock(&job_mutex) == 0); //unlock before destroying
+  assert(pthread_mutex_unlock(&job_mutex) == 0); // Unlock before destroying.
   assert(pthread_mutex_destroy(&job_mutex) == 0);
   return 0;
 }
 
 int job_queue_push(struct job_queue *job_queue, void *data) {
-  printf("push\n");
+  printf("push\n"); // HUSK AT FJERNE.
   assert (pthread_mutex_lock(&job_mutex) == 0);
   if (job_queue->die == 1) {
     err(1, "pushing to destroyed queue");
-    return 0;
+    return 1;
   }
   while (job_queue->capacity <= job_queue->size) {
     printf("waiting for less full queue");
     assert(pthread_cond_wait(&empty_cond, &job_mutex) == 0);
   }
-  job_queue->bottom ++;
-  job_queue->data[job_queue->bottom] = data;
-  if (job_queue->size == 0) {
-    job_queue->top ++;
+  if (job_queue->size != 0) {
+    job_queue->bottom++;
   }
+  job_queue->data[job_queue->bottom] = data;
   job_queue->size ++;
   assert(pthread_mutex_unlock(&job_mutex) == 0);
   assert(pthread_cond_broadcast(&filled_cond) == 0);
@@ -68,7 +67,7 @@ int job_queue_pop(struct job_queue *job_queue, void **data) {
   if (job_queue->size == 0 && job_queue->die == 1) {
     return -1;
   }
-  *data = job_queue->data[job_queue->top]; //pointer??
+  *data = job_queue->data[job_queue->top]; // Pointer??
   if (job_queue->top != job_queue->bottom) {
     job_queue->top ++;
   }
